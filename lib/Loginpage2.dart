cdc623a8285c6e6c6b49c5d009bb5644cd7087ca
jawad <kadhimj095@gmail.com>
2024-09-21 23:19:32 +0300

    showDialog

--- lib/Loginpage2.dart
+++ lib/Loginpage2.dart
@@ -1,108 +1,200 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart'; // استيراد المكتبة
+import 'package:flutter_svg/flutter_svg.dart';
 
-class LoginPage2 extends StatelessWidget {
-  
+class LoginPage2 extends StatefulWidget {
   @override
-  Widget build(BuildContext context) 
-  
-  {
-    
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              // Placeholder for the illustration
-              SizedBox(height:50 ,),
-              Container(
-                height: 150,
-                child: SvgPicture.asset(
-                  'images/undraw_login_re_4vu2.svg', // وضع الصورة SVG هنا
-                  fit: BoxFit.contain,
+  _LoginPage2State createState() => _LoginPage2State();
+}
+
+class _LoginPage2State extends State<LoginPage2> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  bool isPasswordVisible = false;
+  String errorMessage = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 40),
+            Container(
+              height: 150,
+              child: SvgPicture.asset(
+                'images/undraw_access_account_re_8spm.svg', 
+                fit: BoxFit.contain,
+              ),
+            ),
+            const Text(
+              'EDUCATION IS FREE',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Color(0xff3F3D56),
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 100),
+
+            // Email TextField with user icon
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  prefixIcon: Icon(Icons.person),
                 ),
               ),
-              // Title text
-              const Text(
-                'EDUCATION IS FREE',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color:Color(0xff3F3D56),
-                ),
-                textAlign: TextAlign.center,
-              ),
-              // Subtitle text
-              Text(
-                'Online education has never been this easy. Get your Account now and let\'s get started already.',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey[700],
-                ),
-                textAlign: TextAlign.center,
-              ),
-              // Container for buttons
-              Container(
-                height: MediaQuery.of(context).size.height / 3,
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                  color: Color(0xff135A7B),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(150),
-                    topRight: Radius.circular(150),
-                    
+            ),
+            SizedBox(height: 10),
+
+            // Password TextField with visibility toggle
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: TextField(
+                controller: passwordController,
+                obscureText: !isPasswordVisible, 
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        isPasswordVisible = !isPasswordVisible;
+                      });
+                    },
                   ),
                 ),
-                padding: EdgeInsets.only(bottom: 20), // إضافة مساحة أسفل الزر
-                child: Column(
-                  children: [
-                    SizedBox(height: 70),
-                    ElevatedButton(
-                      onPressed: () {
-                        // تنفيذ ما يحدث عند الضغط على زر Sign Up
-                      },
-                      child: Text('SIGN UP',
-                         style: TextStyle(
-                         color: Colors.black,
-                      ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        
-                          shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15), // تعديل زوايا الزر
-                        ),
-                        minimumSize: Size(200, 50),
-                        // تحديد الحجم الأدنى للزر
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Text('OR', style: TextStyle(color: Colors.grey)),
-                    SizedBox(height: 10),
-                    ElevatedButton(
-                      onPressed: () {
-                        // تنفيذ ما يحدث عند الضغط على زر Log In
-                      },
-                      child: Text('LOGIN IN',
-                      style: TextStyle(
-                         color: Colors.black,
-                      ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        
-                          shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15), // تعديل زوايا الزر
-                        ),
-                        minimumSize: Size(200, 50), // تحديد الحجم الأدنى للزر
-                      ),
-                    ),
-                  ],
+              ),
+            ),
+
+            if (errorMessage.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                child: Text(
+                  errorMessage,
+                  style: TextStyle(color: Colors.red),
                 ),
               ),
-            ],
-          ),
+            SizedBox(height: 30),
+
+            // Container for buttons
+            Container(
+              height: MediaQuery.of(context).size.height / 3,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: Color(0xff135A7B),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(350),
+                  topRight: Radius.circular(350),
+                ),
+              ),
+              padding: EdgeInsets.only(bottom: 20),
+              child: Column(
+                children: [
+                  SizedBox(height: 60),
+                  ElevatedButton(
+                    onPressed: () {
+                      // التحقق من صحة البيانات
+                      if (emailController.text.isEmpty || passwordController.text.isEmpty) {
+                        setState(() {
+                          errorMessage = 'Please enter both email and password';
+                        });
+                      } else {
+                        // عرض رسالة نجاح تسجيل الدخول
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text('Success'),
+                              content: Text('أحسنت، تم تسجيل الدخول!'),
+                              actions: <Widget>[
+                                TextButton(
+                                  child: Text('OK'),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      }
+                    },
+                    child: Text(
+                      'LOGIN IN',
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      minimumSize: Size(200, 50),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "Don't have an account? ",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        WidgetSpan(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => CreateAccountPage(),
+                                ),
+                              );
+                            },
+                            child: Text(
+                              "Create one now",
+                              style: TextStyle(
+                                color: const Color.fromARGB(255, 6, 208, 239),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class CreateAccountPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Create Account'),
+      ),
+      body: Center(
+        child: Text('Create Account Page'),
       ),
     );
   }
